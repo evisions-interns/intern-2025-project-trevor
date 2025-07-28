646b19df25944ae1538e653003152194b11b62c6
Trevor Harwell <trevor.harwell@evisions.com>
2025-07-28 19:39:51 +0000

    Added tool calling example

--- src/app/chat/endpoint/route.ts
+++ src/app/chat/endpoint/route.ts
@@ -1,6 +1,7 @@
 import { createOpenRouter } from "@openrouter/ai-sdk-provider";
 import { env } from "@/env";
-import { streamText, type Message } from "ai";
+import { streamText, tool, type Message } from "ai";
+import z from "zod";
 
 const openrouter = createOpenRouter({
   apiKey: env.OPENROUTER_API_KEY,
@@ -14,8 +15,24 @@ export async function POST(request: Request) {
   const result = streamText({
     model: openrouter("openai/gpt-4o-mini"),
     messages,
-    system:
-      "you are a wise and silly gen-z martial arts instructor.  Make your responses short.",
+    system: `you are a wise and silly gen-z martial arts instructor.
+    Make your responses short. Don't use emojis.
+    
+    Use the search tool to lookup information.
+    Only respond with information found in the search tool.
+    `,
+    tools: {
+      search: tool({
+        description: "search knowledge base",
+        parameters: z.object({
+          query: z.string(),
+        }),
+        execute: async () => {
+          return "you login using the big orange button at the button left called 'Sign in'";
+        },
+      }),
+    },
+    maxSteps: 5,
   });
 
   return result.toDataStreamResponse();
