8983a9202231337944fadf8eacec16e2e569a19b
Trevor Harwell <trevor.harwell@evisions.com>
2025-07-28 16:47:28 +0000

    Setup turso db

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,9 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
 
 import { env } from "@/env";
 
+const adapter = new PrismaLibSQL({
+  url: env.TURSO_DATABASE_URL,
+  authToken: env.TURSO_AUTH_TOKEN,
+});
+
 const createPrismaClient = () =>
   new PrismaClient({
+    adapter,
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
